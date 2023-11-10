a21f5944f533f179e4a44494f5622c09b0682fe2
Seraffim <89697694+Serafimklm@users.noreply.github.com>
2023-11-10 02:04:58 -0300

    comentarios

--- IF_elseif_else.c
+++ IF_elseif_else.c
@@ -6,11 +6,13 @@ int main (){
     
     printf ("Insira sua nota: \n");
     scanf("%f", &nota);
-
+    /*if equivale ao (se do portugol)*/
     if (nota >= 7.0){
         printf("aluno aprovado \n");
+    /*else equivale ao  sen?o se em portugol */
     } else if (nota >=5.0 ) {
         printf("aluno em recuperacao \n");
+    /*o else abaixo equivale ao senao*/
     } else {
         printf ("aluno reprovado. \n");
     }
