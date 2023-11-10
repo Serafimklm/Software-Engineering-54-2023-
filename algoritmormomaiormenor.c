ce6b0806e0ba90af39a9184d5c8528284b7a6b31
Seraffim <89697694+Serafimklm@users.noreply.github.com>
2023-11-10 01:33:09 -0300

    algoritimo com strutura else

---
+++ algoritmormomaiormenor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    float m;
+
+    printf ("insira a nota: \n");
+    scanf ("%f", &m);
+
+    if (m >= 7.0){
+        printf ("aluno aprovado \n");
+    } else {
+    printf ("aluno reprovado \n");
+    }
+
+}  
