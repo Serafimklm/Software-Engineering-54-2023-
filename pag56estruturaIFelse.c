2c45470fba4118d93d4d725d3122ddddecf1f9ed
Seraffim <89697694+Serafimklm@users.noreply.github.com>
2023-12-06 23:39:26 -0300

    NUMERO IMPAR NUMERO PAR pag56estruturaIFelse

---
+++ pag56estruturaIFelse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+    int quadrado, num;
+    float raiz;
+
+    printf("\n digite um n?mero inteiro");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        raiz = sqrt (num);
+        printf("\n o numero ? par");
+        printf("\n a raiz quadrada ? %.3f", raiz);
+    }
+    else
+    {
+        quadrado = num * num;
+        printf("\n o numero ? impar");
+        printf("o n?mero ao quadrado ? igual a %d", quadrado);
+    }   
+}
