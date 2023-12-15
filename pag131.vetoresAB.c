b29904e68bdd2fae7f6e88f4a50d1cb6a9db99e3
Seraffim <89697694+Serafimklm@users.noreply.github.com>
2023-12-15 00:53:09 -0300

    programa que le valores dos vetore A e B e imprime em ordem crescente

---
+++ pag131.vetoresAB.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main ()
+ {
+    int vetorA[30], vetorB[40], vetorC[33];
+    int i, j, troca;
+    for (i=0; i<30; i++)
+    {
+        printf("Digite o %d elemento do vetor A:", i); 
+        scanf("%d", &vetorA[i]);
+        printf("Digite o %d elemento do vetor B:", i);
+        scanf("%d", &vetorB[i]);
+        vetorC[i] = vetorA[i] + vetorB[i];
+    }
+    for (i = 0; i < 29; i++)
+    {
+        for (j = i + 1; j < 30; j++)
+        {
+            if (vetorC[i] > vetorC[j])
+            {
+                troca = vetorC[i];
+                vetorC[i] = vetorC[j];
+                vetorC[j] = troca;
+            }
+        }
+    }
+    for ( i = 0; i < 30; i++)
+    {
+        printf("%d-", vetorC[i]);
+    }
+ return (0);   
+ }
