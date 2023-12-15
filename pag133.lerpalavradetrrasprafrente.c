62106260f48c77382acf0e14d6661f44dcf42358
Seraffim <89697694+Serafimklm@users.noreply.github.com>
2023-12-15 01:06:50 -0300

    programa

---
+++ pag133.lerpalavradetrrasprafrente.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main (){
+    
+    char palavra[30];
+    int tam, i;
+    printf("digite a palavra\n");
+    gets(palavra);
+    tam = strlen (palavra);
+    for (i=tam -1; i >=0; i--)
+    {
+        printf ("%c", palavra [i]);
+    }
+ return (0);
+}
